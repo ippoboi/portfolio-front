4262c538bd98c87ee2ba7e2bf849f7760b403521
ippoboi <idimitak@gmail.com>
2023-08-24 12:38:23 +0300

    footer socials

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,10 @@
+import { BiLogoInstagram, BiLogoLinkedin, BiLogoGithub } from "react-icons/bi";
+
 const Footer = () => {
   return (
     <div className="sticky bg-primary-black w-full">
       <div className="xl:max-w-6xl lg:max-w-4xl md:max-w-2xl max-w-xs mx-auto py-5 px-5 flex md:flex-row flex-col gap-16 md:gap-0 justify-between md:items-center items-start mt-10">
-        <div className="text-secondary-white">
+        <div className="text-secondary-white text-center w-full md:text-left md:w-fit">
           <p className="small_title">
             Dimitar <br /> Dimitrov
           </p>
@@ -11,12 +13,25 @@ const Footer = () => {
             <br /> WITH A WEBSITE
           </p>
         </div>
-        <div className="text-white w-full text-center">
-          <p>Socials</p>
+        <div className="flex flex-col items-center mx-auto md:mx-0 ">
+          <p className="text-center text-white small_spaced_text my-5">
+            Join me on social medias !
+          </p>
+          <div className="text-white text-center flex justify-center gap-10">
+            <a href="">
+              <BiLogoInstagram size={25} />
+            </a>
+            <a href="">
+              <BiLogoLinkedin size={25} />
+            </a>
+            <a href="">
+              <BiLogoGithub size={25} />
+            </a>
+          </div>
         </div>
       </div>
       <div>
-        <p className="text-secondary-white text-center tracking-small-text uppercase mb-[18px] font-light text-[10px] lg:text-[12px] font-light mt-[50px]">
+        <p className="text-secondary-white text-center tracking-small-text uppercase mb-[18px] font-light text-[10px] lg:text-[12px] mt-[50px]">
           COPYRIGHT - DIMITAR DIMITROV @2023
         </p>
       </div>
